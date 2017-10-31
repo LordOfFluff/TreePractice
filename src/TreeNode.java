d7afdbca57e6e7b60391ab6181b84e8fef87f7c7
Johnny Chavez <LordOfFluff@users.noreply.github.com>
2017-10-31 16:17:56 -0500

    Completed TreeNode Class

---
+++ src/TreeNode.java
@@ -0,0 +1,21 @@
+//LeafNode class.
+//
+//Constructor: can create a Node with 0 - 2 children.
+//@authur Johnny Chavez
+public class TreeNode <AnyType>{
+    public AnyType element;
+    public TreeNode R;
+    public TreeNode L;
+
+
+    public TreeNode (AnyType theElement, TreeNode Left, TreeNode Right){
+        element = theElement;
+        L = Left;
+        R = Right;
+    }
+
+    public TreeNode (AnyType theElement){
+        this(theElement, null,null);
+    }
+
+}
